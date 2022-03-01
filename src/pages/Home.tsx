275e82285f08d7e0d80af5d2db6e3ff7211ba216
Victor Medeiros <victormedeiros.pj@gmail.com>
2022-03-01 14:04:17 -0300

    completed challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks([...tasks, {id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false}]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = [...tasks];
+    const task = updatedTasks.find((task) => {
+      return task.id === id
+    });
+    if(task) {
+      task.done = !task.done;
+    }
+    setTasks(updatedTasks);
+
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newTasks = tasks.filter((task) => {
+      return task.id !== id;
+    })
+
+    setTasks(newTasks);
   }
 
   return (
